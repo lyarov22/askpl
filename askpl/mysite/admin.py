30012b9c05d68507d644c42058e86db18707f68a
makkenzo <nekgo2009@gmail.com>
2023-10-09 13:54:37 +0600

    structure fix

--- askpl/mysite/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UUIDEntry  # Импортируйте модель UUIDEntry из вашего приложения
-
-@admin.register(UUIDEntry)
-class UUIDEntryAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'name', 'surname', 'group')  # Определите, какие поля будут отображаться в списке записей в админ-панели
-    search_fields = ('uuid', 'name', 'surname', 'group')  # Добавьте поля для поиска
