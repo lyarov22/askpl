371cf54bedeb27775e4f2771ea1059ca51292e94
Фёдоров Илья <dyudyafed006@gmail.com>
2023-10-21 13:53:35 +0600

    last lesson code

--- Arduino/esp32RfidWifiTest/esp32RfidWifiTest.ino
+++ Arduino/esp32RfidWifiTest/esp32RfidWifiTest.ino
@@ -5,27 +5,37 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
+#define RST_PIN 22
+#define SS_PIN 21
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
 const char* ssid[] = {"OPPO_A74", "ssid2", "ssid3"}; 
 const char* password[] = {"04060708", "password2", "password3"};
 const int numberOfNetworks = 3; 
 
-const char* serverUrl = "http://192.168.14.253:5000/api/users/register";
+const char* serverUrl = "http://192.168.18.243:5000/api/users/register";
 
 void setup() {
   Serial.begin(115200);
-
+  SPI.begin();
+  mfrc522.PCD_Init();
+  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+  mfrc522.PCD_AntennaOff();
+  mfrc522.PCD_AntennaOn();
+  
   for (int i = 0; i < numberOfNetworks; i++) {
     WiFi.begin(ssid[i], password[i]);
     Serial.print("Attempting to connect to ");
     Serial.println(ssid[i]);
     
     int attempts = 0;
-    while (WiFi.status() != WL_CONNECTED && attempts < 20) {  
+    while (WiFi.status() != WL_CONNECTED && attempts < 20) { 
       delay(500);
       Serial.print(".");
       attempts++;
     }
-
+  
     if (WiFi.status() == WL_CONNECTED) {
       Serial.println("");
       Serial.println("WiFi connected");
@@ -37,7 +47,7 @@ void setup() {
       Serial.println("Connection failed");
     }
   }
-
+  
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("Could not connect to any of the specified networks.");
   }
@@ -52,23 +62,23 @@ void loop() {
       }
       Serial.print(F("UID: "));
       Serial.println(uid);
-
+  
       DynamicJsonDocument jsonDoc(128);
       jsonDoc["uid"] = uid;
-
+  
       String jsonString;
       serializeJson(jsonDoc, jsonString);
-
+  
       HTTPClient http;
-
+  
       Serial.println("Sending POST request to the server...");
-
+  
       http.begin(serverUrl);
-
+  
       http.addHeader("Content-Type", "application/json");
-
+  
       int httpResponseCode = http.POST(jsonString);
-
+  
       if (httpResponseCode > 0) {
         String response = http.getString();
         Serial.print("Response from the server: ");
@@ -77,10 +87,10 @@ void loop() {
         Serial.print("HTTP request error: ");
         Serial.println(httpResponseCode);
       }
-
+  
       http.end();
       delay(1000);
-
+  
       mfrc522.PICC_HaltA();
     }
   }
